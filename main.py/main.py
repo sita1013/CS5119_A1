9aa8cc4ec7c62a56ee5ee8aeea2038183fda635a
sita1013 <seluvaiasariahita@gmail.com>
2025-03-28 13:10:34 +0000

    added info to txt and file_songs to main code

--- main.py/main.py
+++ main.py/main.py
@@ -1,21 +1,32 @@
 class Music:
-  def __init__(self, artist, song_title, length, genre, average_rating):
-    self.artist = artist
-    self.song_title = song_title
-    self.length = length
-    self.genre = genre
-    self.average_rating = average_rating
+    def __init__(self, artist, song_title, length, genre, average_rating):
+        self.artist = artist
+        self.song_title = song_title
+        self.length = length
+        self.genre = genre
+        self.average_rating = average_rating
 
-  def check_song(self):
-    user_title = input("Please enter a song title: ")
-    try: 
-      while user_title != self.song_title:
-        print("Unfortunately, that song is not streamable.")
-        user_title = input("Please try another song title: ")
-    finally: 
-      return f"The song, '{user_title}' is streamable.\n The artist is {self.artist}.\n This song is considered {self.genre},\n rated at a {self.average_rating} out of 5,\n and is {self.length} long."
-  
-c1 = Music("Radiohead", "Creep", "238 seconds", "Alternative Rock", 4)
-print(c1.check_song())
+    def display_song(self):
+        user_title = input("Please enter a song title: ")
+        try:
+            while user_title != self.song_title:
+                print("Unfortunately, that song is not streamable.")
+                user_title = input("Please try another song title: ")
+        finally: 
+            return f"The song, '{user_title}' is streamable.\n The artist is {self.artist}.\n This song is considered {self.genre},\n rated at a {self.average_rating} out of 5,\n and is {self.length} long."
+
+    def file_songs(music_data.txt):
+        song_list = []
+        with open(music_data.txt, 'r') as file: 
+            for line in file: 
+                info = line.strip().split(',')
+                if len(parts) == 5:
+                    artist, song_title, length, genre, average_rating = parts
+                    song = Music(artist, song_title, length, genre, average_rating)
+                    song_list.append(song)
+        return song_list
+
+    
+
 
 
